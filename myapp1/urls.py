7ad9a4028dca057974cf484c1b2feb61de527e3a
prajal patidar <prajalpatidar001@gmail.com>
2021-06-05 19:07:43 +0530

    done

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register', views.user_register , name='register'),
     path('_login', views.user_login, name='_login'),
     path('logout', views.user_logout, name='logut'),
+    path('post_blog', views.post_blog, name='post_blog'),
+    path('blog_detail/<int:id>', views.blog_detail, name='blog_detail'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('edit/<int:id>', views.edit, name='edit'),
 ]
--- myapp1/views.py
+++ myapp1/views.py
@@ -3,10 +3,15 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from . models import Blog
+from . forms import Edit_Blog
 # Create your views here.
 
 def index(request):
-    return render(request,'home.html')
+    blog = Blog.objects.all()
+    print(blog)
+    context = {'blogs': blog}
+    return render(request,'home.html',context)
 
 def about(request):
     return render(request,'about.html')
@@ -52,3 +57,38 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('/')
+
+def post_blog(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        category = request.POST.get('category')
+        url = request.POST.get('url')
+        description = request.POST.get('description')
+        blog = Blog(title=title, category=category, url=url, description=description , user_id = request.user)
+        blog.save()
+        messages.success(request,'Blog content successfully posted')
+        return redirect('post_blog')  
+    return render(request,'post_blog.html')
+
+def blog_detail(request,id):
+    blog = Blog.objects.get(id =id)
+    context = {'blog':blog}
+    return render(request,'blog_detail.html',context)
+
+def delete(request,id):
+    blog = Blog.objects.get(id=id)
+    blog.delete()
+    messages.success(request,'Blog has been Deleted')
+    return redirect('/')
+
+def edit(request,id):
+    blog = Blog.objects.get(id=id)
+    editblog = Edit_Blog(instance = blog)
+    if request.method =="POST":
+        form = Edit_Blog(request.POST,instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Blog has been Edited')
+            return redirect('/')
+    return render(request,'edit_blog.html',{'edit_blog' :editblog})
+
