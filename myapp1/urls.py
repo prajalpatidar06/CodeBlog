f44c7bf0cdf35c0aa1a7bad0fca1b073676d1587
prajal patidar <prajalpatidar001@gmail.com>
2021-06-05 14:19:41 +0530

    register done

--- myapp1/urls.py
+++ myapp1/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index , name='index'),
     path('about', views.about , name='about'),
-    path('register', views.user_register , name='user_register'),
-    path('_login', views.user_login, name='user_login'),
+    path('register', views.user_register , name='register'),
+    path('_login', views.user_login, name='_login'),
 ]
--- myapp1/views.py
+++ myapp1/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -9,6 +11,28 @@ def about(request):
     return render(request,'about.html')
 
 def user_register(request):
+    if request.method =='POST':
+        fname = request.POST.get('firstname')
+        lname = request.POST.get('lastname')
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        if pass1 != pass2:
+            messages.warning(request,"Password does not match")
+            return redirect('register')
+        elif User.objects.filter(username=uname).exists():
+            messages.warning(request,'Username already exist , please try different username')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.warning(request,'E-mail already exist , please try different email')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(first_name = fname,last_name = lname,username = uname,email = email,password = pass1)
+            user.save()
+            messages.success(request,'Succesfully registered')
+            return redirect('_login')
+    
     return render(request,'register.html')
 
 def user_login(request):
